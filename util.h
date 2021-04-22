037ade6f4003cc73f7962b209b9a4a443a231c7d
momentine <79717797+momentine@users.noreply.github.com>
2021-04-21 22:13:50 -0700

    Add files via upload

--- util.h
+++ util.h
@@ -1,16 +1,18 @@
-/* util.hpp */
+//
+//  util.h
+//  Project3
+//
+//  Created by Nguyen Truong Duy Khang on 4/15/21.
+//
+
 #ifndef util_h
 #define util_h
-
-#include <iostream>
-#include <string>
-
-// enum declaration types
-enum commands {WRITE, FIND, STOP, NONE };
-// character
-commands current(char c);
-
-// util.cpp
-int getInput(int* source, int* x, int* y, int* destination, int* flag);
-
+#include <vector>
+#include "graph.h"
+#include "heap.h"
+int dijkstra_SP(Graph G,HEAP* heap,int s, int t);
+vector<int> findRoute(Graph G, int s, int t);
+void printPathWeight(Graph G, int t);
+vector<string>split_string(string input);
+void printPath(vector<int> route);
 #endif /* util_h */
